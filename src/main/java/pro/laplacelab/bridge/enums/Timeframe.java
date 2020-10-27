83d6ef9844a920b9b10eadc900540854033b5f8b
Pavel Ravvich <triodjangopiter@yandex.ru>
2020-10-28 01:43:30 +0300

    add all timeframes Fixes #12

--- src/main/java/pro/laplacelab/bridge/enums/Timeframe.java
+++ src/main/java/pro/laplacelab/bridge/enums/Timeframe.java
@@ -1,8 +1,7 @@
 package pro.laplacelab.bridge.enums;
 
 public enum Timeframe {
-    M_1, M_2, M_5, M_10, M_15, M_30,
-    H_1, H_2, H_5, H_10,
-    D_1,
-    MN
+    M_1, M_2, M_3, M_4, M_5, M_6, M_10, M_12, M_15, M_20, M_30,
+    H_1, H_2, H_3, H_4, H_6, H_8, H_12,
+    D_1, W_1, MN
 }
