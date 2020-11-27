4c0b8a3881314ab753fdee217ec49afd273931d3
Pavel Ravvich <triodjangopiter@yandex.ru>
2020-11-27 11:36:23 +0300

    UnitTest for Input constructor. Fixes #71

--- src/main/java/pro/laplacelab/mt4j/exception/InvalidInputException.java
+++
@@ -1,7 +0,0 @@
-package pro.laplacelab.mt4j.exception;
-
-public class InvalidInputException extends RuntimeException {
-    public InvalidInputException() {
-        super("Input can't be constructed. Parameters can't be null or empty.");
-    }
-}
--- src/main/java/pro/laplacelab/mt4j/model/Input.java
+++ src/main/java/pro/laplacelab/mt4j/model/Input.java
@@ -3,22 +3,21 @@ package pro.laplacelab.mt4j.model;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 import pro.laplacelab.mt4j.enums.InputType;
-import pro.laplacelab.mt4j.exception.InvalidInputException;
 
 import javax.validation.constraints.NotEmpty;
 import javax.validation.constraints.NotNull;
-import java.math.BigDecimal;
 import java.time.LocalTime;
 import java.time.format.DateTimeFormatter;
-import java.util.Objects;
 
 @Data
 @EqualsAndHashCode
 @NoArgsConstructor
+@AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Input {
 
@@ -41,23 +40,12 @@ public class Input {
         return value;
     }
 
-    public BigDecimal asBigDecimal() {
-        return new BigDecimal(value);
+    public Double asDouble() {
+        return Double.valueOf(value);
     }
 
     public LocalTime asLocalTime() {
         return LocalTime.parse(value, DateTimeFormatter.ofPattern("HH:mm"));
-    }
-
-    public Input(final String key, final String value, final InputType type) {
-        if (Objects.isNull(key) || key.isEmpty() ||
-                Objects.isNull(value) || value.isEmpty() ||
-                Objects.isNull(type)) {
-            throw new InvalidInputException();
-        }
-        this.key = key;
-        this.type = type;
-        this.value = value;
     }
 
 }
