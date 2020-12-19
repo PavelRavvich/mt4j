9da32e65f616926a7ccd5a1b9506900577f217da
Pavel Ravvich <triodjangopiter@yandex.ru>
2020-12-19 17:18:57 +0300

    Get rid from redundant lombok annotations. Fixes #126

--- src/main/java/pro/laplacelab/mt4j/model/Position.java
+++ src/main/java/pro/laplacelab/mt4j/model/Position.java
@@ -4,13 +4,11 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.AllArgsConstructor;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 import pro.laplacelab.mt4j.enums.PositionType;
 
 import javax.validation.constraints.NotNull;
 
 @Data
-@NoArgsConstructor
 @AllArgsConstructor
 @JsonIgnoreProperties(ignoreUnknown = true)
 public class Position  {
